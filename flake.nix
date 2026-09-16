afb1584d8eb009b6fae819eccdea2842579c489c
Alexander Mohr <alexdermohr@gmail.com>
2026-09-16 12:00:10 +0200

    chore: tighten Nixer package scope

--- flake.nix
+++ flake.nix
@@ -27,14 +27,14 @@
         pkgs.stdenvNoCC.mkDerivation {
           pname = "nixer";
           version = "0.1.0";
-          src = self;
+          dontUnpack = true;
           nativeBuildInputs = [ pkgs.makeWrapper ];
           dontBuild = true;
 
           installPhase = ''
             runHook preInstall
             mkdir -p "$out/bin" "$out/libexec/nixer"
-            cp server.py "$out/libexec/nixer/server.py"
+            cp ${./server.py} "$out/libexec/nixer/server.py"
             makeWrapper ${python}/bin/python "$out/bin/nixer" \
               --add-flags "$out/libexec/nixer/server.py"
             runHook postInstall
@@ -43,7 +43,7 @@
           meta = {
             description = "Nix-only specialist MCP for Nix, Nixpkgs and NixOS";
             mainProgram = "nixer";
-            platforms = nixpkgs.lib.platforms.linux;
+            platforms = supportedSystems;
           };
         };
     in
@@ -58,9 +58,16 @@
       });
 
       nixosModules.default = { config, lib, pkgs, ... }:
+        let
+          system = pkgs.stdenv.hostPlatform.system;
+        in
         import ./nix/module.nix {
           inherit config lib pkgs;
-          defaultPackage = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+          defaultPackage =
+            if builtins.elem system supportedSystems then
+              self.packages.${system}.default
+            else
+              throw "Nixer currently supports only: ${builtins.concatStringsSep ", " supportedSystems}";
         };
     };
 }
